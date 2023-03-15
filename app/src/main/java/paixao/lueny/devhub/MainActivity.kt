4decb8617aa8d2530610e6d7a7083c30207e28dc
Lueny Dantas <luenypaixao@gmail.com>
2023-03-15 19:11:48 -0300

    Main Screen Updates

--- app/src/main/java/paixao/lueny/devhub/MainActivity.kt
+++ app/src/main/java/paixao/lueny/devhub/MainActivity.kt
@@ -4,47 +4,48 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
+import coil.compose.AsyncImage
 import kotlinx.coroutines.launch
 import paixao.lueny.devhub.ui.screens.ProfileScreen
 import paixao.lueny.devhub.ui.theme.DevHubTheme
 import paixao.lueny.devhub.ui.webclient.RetrofitInit
+import paixao.lueny.devhub.ui.webclient.model.GitHubWebClient
+import androidx.compose.foundation.layout.Column as Column1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lifecycleScope.launch{
-            RetrofitInit().gitHubService.findProfileBy("lueny-dantas")
-                .let{
-                    Log.i("MainActivity","onCreate: $it")
-                }
-        }
         setContent {
-            App()
+            DevHubTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ProfileScreen("lueny-dantas")
+                }
+            }
         }
     }
-}
-
-@Composable
-fun App() {
-    DevHubTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colors.background
-        ) {
-            ProfileScreen()
-        }
-    }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun AppPreview() {
-    App()
 }
