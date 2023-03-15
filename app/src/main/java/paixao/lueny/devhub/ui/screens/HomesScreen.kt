c6245510fac7e748c26897534dfb0b72bc3cef2a
Lueny Dantas <luenypaixao@gmail.com>
2023-03-15 19:12:00 -0300

    Home Screen Updates

--- app/src/main/java/paixao/lueny/devhub/ui/screens/HomesScreen.kt
+++ app/src/main/java/paixao/lueny/devhub/ui/screens/HomesScreen.kt
@@ -1,97 +1,190 @@
 package paixao.lueny.devhub.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import paixao.lueny.devhub.R
-import paixao.lueny.devhub.ui.theme.Purple200
-import paixao.lueny.devhub.ui.theme.Teal200
+import paixao.lueny.devhub.ui.webclient.model.GitHubRepository
+import paixao.lueny.devhub.ui.webclient.model.GitHubWebClient
 
 @Composable
+fun ProfileScreen(
+    user: String,
+    webClient: GitHubWebClient = GitHubWebClient()
+) {
+    val uiState = webClient.uiState
+    LaunchedEffect(null) {
+        webClient.findProfileBy(user)
+    }
+    Profile(uiState)
 
-fun ProfileScreen() {
-    Column(
-        Modifier
-            .heightIn(250.dp, 300.dp)
-            .width(200.dp)
-    ) {
+}
+
+@Composable
+fun Profile(uiState: ProfileUiState) {
+    LazyColumn {
+        item {
+            ProfileHeader(uiState)
+        }
+        item {
+            if (uiState.repositories.isNotEmpty()) {
+                Text(
+                    text = "Repositórios", Modifier.padding(8.dp),
+                    fontSize = 24.sp
+                )
+            }
+        }
+        items(uiState.repositories) { repo ->
+            RepositoryItem(repo = repo)
+        }
+    }
+}
+
+@Composable
+private fun ProfileHeader(state: ProfileUiState) {
+    Column {
+        val boxHeight = remember { 150.dp }
+        val imageHeight = remember { boxHeight }
+
         Box(
             modifier = Modifier
-                .height(100.dp)
+                .fillMaxWidth()
                 .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(Purple200, Teal200)
+                    Color(0xFF2d333b), shape = RoundedCornerShape(
+                        bottomStart = 16.dp,
+                        bottomEnd = 16.dp
                     )
                 )
-                .fillMaxWidth()
+                .height(boxHeight)
         ) {
             AsyncImage(
-                "https://avatars.githubusercontent.com/u/98789294?v=4",
-                contentDescription = "foto do perfil",
-                placeholder = painterResource(id = R.drawable.user_placeholder),
+                state.image,
+                contentDescription = "userProfile pic",
+                placeholder = painterResource(R.drawable.user_placeholder),
                 modifier = Modifier
-                    .size(100.dp)
-                    .offset(y = 100.dp / 2)
-                    .clip(shape = CircleShape)
+                    .offset(y = imageHeight / 2)
+                    .size(imageHeight)
                     .align(Alignment.BottomCenter)
+                    .clip(CircleShape),
             )
         }
-        Spacer(Modifier.height(100.dp / 2))
+        Spacer(modifier = Modifier.height(imageHeight / 2))
         Column(
             Modifier
-                .padding(16.dp)
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Lueny Dantas",
+                state.name,
+                fontSize = 32.sp
+            )
+            Text(
+                state.user,
                 fontSize = 18.sp,
-                fontWeight = FontWeight(700),
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-
-            )
-            Text(
-                text = "lueny-dantas",
-                fontSize = 14.sp,
-                fontWeight = FontWeight(700),
-                maxLines = 2,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
-            Text(
-                text = "Desenvolvedora Android",
-                fontSize = 14.sp,
-                fontWeight = FontWeight(400),
-                maxLines = 2,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
+                fontWeight = FontWeight.Bold
             )
         }
+        Text(
+            state.bio,
+            Modifier
+                .padding(
+                    start = 8.dp,
+                    bottom = 8.dp,
+                    end = 8.dp
+                )
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+    }
+}
 
+@Composable
+fun RepositoryItem(repo: GitHubRepository) {
+    Card(
+        modifier = Modifier.padding(8.dp),
+        elevation = 4.dp
+    ) {
+        Column {
+            Text(
+                repo.name,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color(0xFF2d333b))
+                    .padding(8.dp),
+                fontSize = 24.sp,
+                color = Color.White
+            )
+            if (repo.description.isNotBlank()) {
+                Text(
+                    repo.description,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                )
+            }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun InformationProfilePreview() {
-    ProfileScreen()
+fun RepositoryItemPreview() {
+    RepositoryItem(
+        repo = GitHubRepository(
+            name = "lueny-dantas",
+            description = "Android Developer Jr"
+        )
+    )
 }
+
+@Preview(showBackground = true)
+@Composable
+fun ProfileWithRepositoriesPreview() {
+    Profile(
+        uiState = ProfileUiState(
+            user = "lueny-dantas",
+            image = "https://avatars.githubusercontent.com/u/8989346?v=4",
+            name = "Lueny Dantas",
+            bio = "Android Developer",
+            repositories = listOf(
+                GitHubRepository(name = "github-compose"),
+                GitHubRepository(
+                    name = "ceep-compose",
+                    description = "Sample project to practice the Jetpack Compose Apps"
+                ),
+                GitHubRepository(
+                    name = "orgs-jetpack-compose",
+                    description = "Projeto de simulação do e-commerce de produtos naturais com a finalidade de treinar o Jetpack Compose"
+                )
+            )
+        )
+    )
+}
+
+data class ProfileUiState(
+    val user: String = "",
+    val image: String = "",
+    val name: String = "",
+    val bio: String = "",
+    val repositories: List<GitHubRepository> = emptyList()
+)
+
+
