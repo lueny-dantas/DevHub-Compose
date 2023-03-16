70c23f00675712648a20055c9d0d38bb7b15e434
Lueny Dantas <luenypaixao@gmail.com>
2023-03-15 22:37:29 -0300

    Main code restructuring

--- app/src/main/java/paixao/lueny/devhub/MainActivity.kt
+++ app/src/main/java/paixao/lueny/devhub/MainActivity.kt
@@ -1,38 +1,14 @@
 package paixao.lueny.devhub
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.lifecycleScope
-import coil.compose.AsyncImage
-import kotlinx.coroutines.launch
 import paixao.lueny.devhub.ui.screens.ProfileScreen
 import paixao.lueny.devhub.ui.theme.DevHubTheme
-import paixao.lueny.devhub.ui.webclient.RetrofitInit
-import paixao.lueny.devhub.ui.webclient.model.GitHubWebClient
-import androidx.compose.foundation.layout.Column as Column1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
